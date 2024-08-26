8db480ea7fbacee2789e2a8358f2893237e3553d
Adib <Adibdb1003@gmail.com>
2024-08-26 12:06:27 +0800

    convert to ts

--- server.ts
+++ server.ts
@@ -1,22 +1,32 @@
-const express = require("express");
-require("dotenv").config();
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
 const app = express();
 const PORT = 5000;
-const mongoose = require("mongoose");
-const cors = require("cors");
 const { DB_URL } = process.env;
 
+// Middleware setup
 app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 
-mongoose.connect(`${DB_URL}`);
+// MongoDB connection
+mongoose
+  .connect(`${DB_URL}`)
+  .then(() => console.log("Connected to Database"))
+  .catch((err) => console.error("Database connection error:", err));
 
+// Route handlers
 app.use("/profile", require("./controllers/users"));
 app.use("/habit", require("./controllers/habits"));
 app.use("/challenge", require("./controllers/challenges"));
 app.use("/badge", require("./controllers/badges"));
 app.use("/quote", require("./controllers/quotes"));
 
-mongoose.connection.once("open", () => console.log("Connected to Database"));
+// Start the server
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
