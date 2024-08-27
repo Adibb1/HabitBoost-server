15c9cdc29c65ea5d2faf263cf78ff26684d9d7ff
Adib <Adibdb1003@gmail.com>
2024-08-27 09:29:32 +0800

    helo

--- server.ts
+++ server.ts
@@ -1,16 +1,23 @@
-const express = require("express");
-require("dotenv").config();
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import path from "path";
+
+dotenv.config();
+
 const app = express();
-const PORT = 5000;
-const mongoose = require("mongoose");
-const cors = require("cors");
+const PORT = process.env.PORT || 5000;
 const { DB_URL } = process.env;
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
-mongoose.connect("mongodb://localhost/morse-master");
+mongoose.connect(DB_URL || "mongodb://localhost/morse-master", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} as mongoose.ConnectOptions);
 
 app.use("/profile", require("./controllers/users"));
 app.use("/habit", require("./controllers/habits"));
