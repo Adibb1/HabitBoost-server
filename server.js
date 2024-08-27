a21295c4f4c113036bf9cd7191063d8282aa1e9a
Adib <Adibdb1003@gmail.com>
2024-08-27 09:20:55 +0800

    hi

--- server.js
+++ server.js
@@ -1,32 +1,22 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-
-// Load environment variables
-dotenv.config();
-
+const express = require("express");
+require("dotenv").config();
 const app = express();
 const PORT = 5000;
+const mongoose = require("mongoose");
+const cors = require("cors");
 const { DB_URL } = process.env;
 
-// Middleware setup
 app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 
-// MongoDB connection
-mongoose
-  .connect(`${DB_URL}`)
-  .then(() => console.log("Connected to Database"))
-  .catch((err: any) => console.error("Database connection error:", err));
+mongoose.connect("mongodb://localhost/morse-master");
 
-// Route handlers
 app.use("/profile", require("./controllers/users"));
 app.use("/habit", require("./controllers/habits"));
 app.use("/challenge", require("./controllers/challenges"));
 app.use("/badge", require("./controllers/badges"));
 app.use("/quote", require("./controllers/quotes"));
 
-// Start the server
+mongoose.connection.once("open", () => console.log("Connected to Database"));
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
